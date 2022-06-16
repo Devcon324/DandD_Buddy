204ff32b8632ff3fc2528072308c50b73eece054
Devon Knight <devknight324@gmail.com>
2022-06-16 13:00:03 -0400

    remove dice

--- RPGbot.py
+++ RPGbot.py
@@ -25,30 +25,13 @@ async def on_message(message):
         await message.channel.send('Hello!')
 
 
-description = '''An example bot to showcase the discord.ext.commands extension
-module.
-There are a number of utility commands being showcased here.'''
-intents = discord.Intents.default()
-intents.members = True
-
-bot = commands.Bot(command_prefix='?', description=description, intents=intents)
-
-@bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
 
 
 
 
 
 
-bot.run('OTg2ODMzOTU1NTMxNjUzMTUw.Gma0XO.7cVBa6U6VhKMgwp-Zssm3EjYk9x96GSxZrMSNg')
+
+
+
 client.run('OTg2ODMzOTU1NTMxNjUzMTUw.Gma0XO.7cVBa6U6VhKMgwp-Zssm3EjYk9x96GSxZrMSNg')
