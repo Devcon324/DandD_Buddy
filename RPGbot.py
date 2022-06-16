861b2ce9e1d86421305d414631c4c35c26af3c8b
Devon Knight <devknight324@gmail.com>
2022-06-16 13:03:55 -0400

    re

--- RPGbot.py
+++ RPGbot.py
@@ -21,7 +21,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
 
 
