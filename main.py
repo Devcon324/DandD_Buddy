f946fdb413197f40bb5ec07cbd39fe74ca6eed5c
Devon Knight <devknight324@gmail.com>
2022-06-22 21:09:38 -0400

    Music player works to play files in Audio folder

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from discord.ext import commands, tasks
 from discord import FFmpegPCMAudio
 import random
 import operator
+from titlecard import titlecard
+from apikeys import *
+import requests
+import json
 import os
 import asyncio
 import youtube_dl
 import time
-from titlecard import titlecard
-from apikeys import *
 
 #setup
 ops = {
@@ -19,13 +21,18 @@ ops = {
     '%' : operator.mod,
     '^' : operator.xor,
 }
+
+intents = discord.Intents.default()
+intents.members = True
+
 client = discord.Client()
-bot = commands.Bot(command_prefix='!', case_insensitive=True)
+bot = commands.Bot(command_prefix='!', case_insensitive=True, intents=intents)
 
 @bot.event
 async def on_ready():
     titlecard(bot)
 
+# greets the user
 @bot.command()
 async def hello(ctx):
     greetings = [
@@ -35,11 +42,23 @@ async def hello(ctx):
         'Hi',
         'Bonjour',
         'Hello there, ']
-    await ctx.channel.send(f"{random.choice(greetings)} {str(ctx.author).split('#')[0]} I am {bot.user}")
+    await ctx.channel.send(f"{random.choice(greetings)} {str(ctx.author).split('#')[0]}, I am {bot.user}")
+
+# greets new memebers
+@bot.event
+async def on_member_join(member):
+    channel = client.get_channel(986834387968598049)
+    await channel.send(f"Hi {str(member.name).split('#')[0]}, welcome to the server")
+
+# says Goodbye to leaving memebers
+@bot.event
+async def on_member_remove(member):
+    channel = client.get_channel(986834387968598049)
+    await channel.send(f"Goodbye {str(member.name).split('#')[0]}")
 
 # takes !roll argument, case insensitive
 # Examples: "!roll 1d20", "!roll 1d20 + 5"
-@bot.command()
+@bot.command(pass_context = True)
 async def roll(ctx, *args):
     dice = str.lower(args[0])
     name = str(ctx.author).split("#")[0]
@@ -86,24 +105,77 @@ async def roll(ctx, *args):
             await ctx.channel.send("**🎉 " + name + " Got a Natural 20!**", tts=True)
 
 # bot joins user's channel && Plays an audio file
-@bot.command()
+@bot.command(pass_context = True)
 async def join(ctx):
     if (ctx.author.voice):
         channel = ctx.message.author.voice.channel
         voice = await channel.connect()
-        source = FFmpegPCMAudio('Audio\kill-kill.mp3')
-        voice.play(source)
+        source = FFmpegPCMAudio('Audio\Bot_joined.wav')
+        player = voice.play(source)
     else:
         await ctx.send('You are not connected to a voice channel for me to join!')
     
 # bot leaves user's voice channel
-@bot.command()
+@bot.command(pass_context = True)
 async def leave(ctx):
     if (ctx.voice_client):
         await ctx.guild.voice_client.disconnect()
         await ctx.send('I left the voice channel')
     else:
         await ctx.send("I am not in a voice channel")
+
+
+
+
+
+
+
+"""Music Bot Commands"""
+@bot.command(pass_context = True)
+async def pause(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild = ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send("No audio is playing in the voice channel")
+
+@bot.command(pass_context = True)
+async def resume(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild = ctx.guild)
+    if voice.is_paused():
+        voice.resume()
+    else:
+        await ctx.send("No audio is paused in the voice channel")
+
+@bot.command(pass_context = True)
+async def stop(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild = ctx.guild)
+    voice.stop()
+
+# !play "Arg" will play a file in Audio folder using filename as arg
+# added fucntionality to pass filename, no extension neeeded
+# function loops through audio folder and plays relevant audio
+@bot.command(pass_context = True)
+async def play(ctx, arg):
+    voice = ctx.guild.voice_client
+    for file in os.listdir('Audio'):
+        if str.lower(arg) == str(file).split('.')[0]:
+            ext = str(file).split('.')[1]
+            source = FFmpegPCMAudio(f'Audio\{arg}.{ext}')
+    player = voice.play(source)
+
+
+@bot.command(pass_context = True)
+async def chill(ctx):
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        voice = await channel.connect()
+        source = FFmpegPCMAudio('Audio\supermarket.mp3')
+        player = voice.play(source)
+    else:
+        await ctx.send('You are not connected to a voice channel for me to join!')
+
+
 
 
 bot.run(BOTTOKEN)
